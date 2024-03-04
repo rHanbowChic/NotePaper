fedca87a89de6ea1701d7c2dc6b9574de4bdaea7
Ect07 <ect07aha@hotmail.com>
2024-03-04 23:41:34 +0800

    add comments

--- app.py
+++ app.py
@@ -25,7 +25,10 @@ VALID_TAGS = ['strong', 'em', 'p', 'ul', 'ol', 'li', 'b', 'i',
 
 # 通过去除除VALID_TAGS外所有的标签与VALID_TAGS标签的所有属性来避免XSS攻击。
 def sanitize_html(text):
+    # 这是BeautifulSoup。
     soup = BeautifulSoup(text, "html.parser")
+    # 遍历所有HTML标签，如果在VALID_TAGS中，则保留标签但删除所有属性（防止诸如onclick等属性的脚本执行）
+    # 如果不在，则删除标签（在hidden设置为True时输出文本）
     for tag in soup.findAll(True):
         if tag.name in VALID_TAGS:
             lst = []
@@ -55,9 +58,12 @@ def close_connection(exception):
         db.close()
 
 
+# 笔记页面的GET方法。使用浏览器访问笔记页面（如http://hostname/a）时触发。
 @app.route("/<page>", methods=['GET'])
 def page_get(page):
+    # 获得一个执行SQL语句的SQlite cursor。get_db是在前文声明的。
     cur = get_db().cursor()
+    # 如果以.md结尾，则渲染markdown并输出HTML作为模板的一部分。（templates/note_md.html）
     if page.endswith(".md"):  # /example1.md
         page = page[0:-3]  # example1
         text = cur.execute("select text from pages where id = ?", (page,)).fetchall()
@@ -75,7 +81,7 @@ def page_get(page):
             return Response(text, mimetype='text/plain')
         else:
             return render_template('note_md.html', page=page, text=text)
-    else:
+    else:  # 如果不以.md结尾，则返回笔记页面。（templates/note.html）
         text = cur.execute("select text from pages where id = ?", (page,)).fetchall()
         if len(text) == 0:
             text = ""
@@ -87,13 +93,15 @@ def page_get(page):
             "curl/" in request.headers.get("User-Agent")
             or "Wget/" in request.headers.get("User-Agent")
             or is_text_request
-        )):  # 给带有text参数的请求始终直接显示内容
+        )):  # 给curl,Wget或带有text参数(?text)的请求直接显示内容
             return Response(text, mimetype='text/plain')
         else:
             return render_template('note.html', page=page, text=text)
 
 
-@app.route("/<page>", methods=['POST'])  # 兼容目的
+# 笔记页面的POST方法。在原版Notems中，这是更新笔记的唯一方法。NotePaper使用Socket.IO更新笔记内容，但此方法因兼容目的被保留。
+# 如果你不想要POST更新，可以安全删除@app.route和以下的整个note_post()函数。
+@app.route("/<page>", methods=['POST'])
 def note_post(page):
     cur = get_db().cursor()
     # 正常情况下，浏览器永远不会对以.md结尾的页面发送POST请求。但不排除使用其他程序的情况。
@@ -105,6 +113,7 @@ def note_post(page):
     return ""
 
 
+# 访问根目录时触发。302跳转到一个随机的4位小写字母页面。
 @app.route("/", methods=['GET'])
 def root_redirect():
     randword = ""
@@ -113,14 +122,20 @@ def root_redirect():
     return redirect(f"./{randword}", code=302)
 
 
+# Socket.IO 加入房间。浏览器端的JS在页面完成加载时传递信息，‘page’为所在的页面。例如http://hostname/odyu为‘odyu’。
+# 服务器接收到消息后会加入room。此后服务器可向此页面的room发送消息。
 @socketio.on("join", namespace="/note-ws")
 def join(message): join_room(message['page'])
 
 
+# Socket.IO离开房间。目前此方法永远不会被调用！因为用户关闭窗口时无法执行特定的JS。
+# 但Socket.IO会在浏览器端一段时间无响应后自动离开房间。
 @socketio.on("left", namespace="/note-ws")
 def left(message): leave_room(message['page'])
 
 
+# 浏览器端检测到页面变更时发送的消息，包含页面名与更新的笔记内容。
+# 服务器会将已更新的笔记发送到页面对应room中所有的浏览器端。
 @socketio.on("text_post", namespace="/note-ws")
 def text_post(message):
     room = message['page']
--- static/js/note-socket.js
+++ static/js/note-socket.js
@@ -1,24 +1,26 @@
+// 需要 jQuery 2.2.4 与 SocketIO 4.7.4。在模板'note.html'中，这两个依赖被script标签引用。
+
 var $textarea = $(".content");
-var content = $textarea.val();
-$(".print").text(content);
+$(".print").text($textarea.val());
+// page为页面名。例如http://hostname/odyu为'odyu'。
 const page = decodeURIComponent(window.location.pathname.substring(1));
 
 var socket;
 $(document).ready(function(){
     socket = io.connect('/note-ws');
-
+    // 加入到room
     socket.on('connect', () => {
         socket.emit('join',{'page': page});
     })
-
+    // 接收到服务器的广播后，更新页面内容
     socket.on('text_broadcast', (data) => {
         $textarea.val(data.text);
     });
-
+    // 用户修改页面内容后，发送内容到服务器
     area = document.querySelector('textarea');
     area.addEventListener('input', () => {
         socket.emit('text_post', {'page':page, 'text':$textarea.val()});
-        $(".print").text(content);
+        $(".print").text($textarea.val());
     }, false);
 
 });
