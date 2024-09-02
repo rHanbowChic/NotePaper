32fa5b084ed18c8a885cd78eeab8b51dc2ba294a
Ect07 <ect07aha@hotmail.com>
2024-09-02 19:23:40 +0800

    optimized url regex

--- utils/link.py
+++ utils/link.py
@@ -5,7 +5,7 @@ def auto_link(passage: str):
     """为所有HTTP，HTTPS与站内链接URL自动添加Markdown超链接。"""
     pat_internal = r"\s(\/\w+[^=\s]*)"  # 极度不适
     passage = re.sub(pat_internal, r'[\1](\1)', passage)
-    pat_external = r"(https?:\/\/\w+\.\w+[^=\s]*)"  # 令人失去理智的古老咒文
+    pat_external = r"(https?:\/\/[\w-]+\.[^=\s]*)"  # 令人失去理智的古老咒文
     return re.sub(pat_external, r'[\1](\1)', passage)
 
 
